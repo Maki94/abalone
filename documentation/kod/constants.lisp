042c216100044d934234e91c9a9a2b79e5c6eec7
Maki94 <markomihajlovicfm@gmail.com>
2016-04-28 10:28:58 +0200

    obisan kod koji je poslat profesoru

--- documentation/kod/constants.lisp
+++
@@ -1,9 +0,0 @@
-(require "algorithms.lisp")
-(require "init.lisp")
-
-(defconstant desno 1)
-(defconstant gore-desno 2)
-(defconstant gore-levo 3)
-(defconstant levo 4)
-(defconstant dole-levo 5)
-(defconstant dole-desno 6)
